1816cde1c03de8fe5bf3b53073263c0da028547b
onur koc <onurkottz@gmail.com>
2022-11-02 14:12:36 +0300

    scroll-fedd

--- real-time-chat/src/components/chatlist/ChatList.js
+++ real-time-chat/src/components/chatlist/ChatList.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import style from "../style.module.css"
-import ChatItem from "./ChatItem"
-import {useChat} from "../../context/ChatContext";
+import React from "react";
+import style from "../style.module.css";
+import ChatItem from "./ChatItem";
+import { useChat } from "../../context/ChatContext";
+import ScrollableFeed from "react-scrollable-feed";
 
 function ChatList() {
-  const {messages} = useChat();
-  console.log(messages)
-  
+  const { messages } = useChat();
+  console.log(messages);
+
   // If the messages is listed two times on the list, you should write <React.Fragment> instead of <React.StrictMode> in the index.js
   return (
-    <div className={style.chatList}>
-      <div>
-      {
-        messages.map((item, key) => {
-          return  <ChatItem key={key} item={item} /> 
-        })
-      }  
+    
+      <div className={style.chatList}>
+        <ScrollableFeed forceScroll={true}>
+          {messages.map((item, key) => {
+            return <ChatItem key={key} item={item} />;
+          })}
+        </ScrollableFeed>
       </div>
-    </div>
-  )
+    
+  );
 }
 
-export default ChatList
+export default ChatList;
